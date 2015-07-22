62a8f9bf509e9b5aa31a67b62c7ba74beabc0d16
Bill Mitchell <wkmitch@gmail.com>
2015-07-22 19:46:23 +0100

    After episode 3

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Project Flyer</title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+	<div class="container">
+		@yield('content')
+	</div>
+
+</body>
+</html>
