c492795f23f8b2e037bf544795392b405341b625
Bill Mitchell <wkmitch@gmail.com>
2015-07-28 07:28:01 +0100

    After episode 7

--- app/Flyer.php
+++ app/Flyer.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flyer extends Model
 {
-    
+	/**
+	 * A flyer is composed of many photos.
+	 * 
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+    public function photos()
+    {
+    	return $this->hasMany('App/Photo');	
+    }
 }
---
+++ app/Photo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+
+	protected $table = 'flyer_photos';
+
+	protected $fillable = ['photo'];
+
+    public function flyer()
+    {
+    	return $this->belongsTo('App\Flyer');
+    }
+}
